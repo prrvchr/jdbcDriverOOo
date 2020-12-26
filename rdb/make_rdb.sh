2737e7c2a35553e42ad9ec4ab0e58e9b24172e53
User <prrvchr@gmail.com>
2020-12-26 08:37:05 +0100

    rdb update

--- rdb/make_rdb.sh
+++ rdb/make_rdb.sh
@@ -5,9 +5,7 @@ Path=$(dirname "${0}")
 
 rm -f ${Path}/types.rdb
 
-${Path}/merge_rdb.sh ${OOoPath} com/sun/star/auth/OAuth2Request
 ${Path}/merge_rdb.sh ${OOoPath} com/sun/star/auth/XRestKeyMap
-${Path}/merge_rdb.sh ${OOoPath} com/sun/star/auth/XInteractionUserName
 
 read -p "Press enter to continue"
 
