85db93648a2607c619c28156794d37c361deb880
Pierre <prrvchr@gmail.com>
2023-02-13 01:15:16 +0100

    add Request Exception

--- source/jdbcDriverOOo/service/pythonpath/jdbcdriver/options/optionsmodel.py
+++ source/jdbcDriverOOo/service/pythonpath/jdbcdriver/options/optionsmodel.py
@@ -288,7 +288,7 @@ class OptionsModel(unohelper.Base):
             msg = getMessage(self._ctx, g_message, 141, e.Message)
             logMessage(self._ctx, SEVERE, msg, 'OptionsDialog', '_getDriverVersion()')
         except Exception as e:
-            msg = getMessage(self._ctx, g_message, 142, (e, traceback.print_exc()))
+            msg = getMessage(self._ctx, g_message, 142, e, traceback.print_exc())
             logMessage(self._ctx, SEVERE, msg, 'OptionsDialog', '_getDriverVersion()')
         return version
 
